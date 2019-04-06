85c0c6da5e19d0226019d4582a09a7c929c1ebda
alnahlaw <hadi.alnahlawi@mail.utoronto.ca>
2019-04-05 21:10:03 -0400

    setting up server stuff

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000
 const bodyParser = require('body-parser') // middleware for parsing HTTP body from client
 const session = require('express-session')
 const path = require('path')
+const bcrypt = require('bcryptjs')
+
+
 
 const {
 	ObjectID
@@ -46,6 +49,34 @@ app.use(session({
 app.get('/', (req, res) => {
 	console.log(__dirname)
 	res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/signup', (req, res) =>{
+
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password
+	})
+
+
+	User.find({username: req.body.username}).then((result) => {
+        if(result.length === 0){
+            //res.send(result);
+            
+            user.save().then((result) => {
+                req.session.user = user._id;
+                req.session.email = user.username;
+                res.sendFile(path.join(__dirname, "/home.html"));
+            }).catch((error) => {
+                //res.sendFile(path.join(__dirname, "/public/pages/index.html"));
+
+                res.send(error.message);
+            }); 
+        }else{
+            res.redirect('/');
+        }
+
 })
 
 
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+
+
+const UserSchema = new mongoose.Schema({ 
+	username: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: [8 , 'Password has to be atleast 8 characters'],
+		trim: true
+	},
+	isAdmin: {
+		type: Number,
+		default: false,
+	}
+});
+
+
+UserSchema.pre('save', function(next) {
+	const user = this;
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (error, salt) => {
+			bcrypt.hash(user.password, salt, (error, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { 
+	User 
+};
