ea7ce8c79027755441626aa214d12e73ed9c0405
alnahlaw <hadi.alnahlawi@mail.utoronto.ca>
2019-04-06 15:18:24 -0400

    added some dom manipulation

---
+++ public/scripts/home.js
@@ -0,0 +1,49 @@
+
+
+const postButton = document.getElementById('post-button')
+
+postButton.addEventListener('click', addComment)
+
+var commentSection = document.getElementById('comment-section')
+
+const refreshButton = document.getElementById("refresh-div")
+
+refreshButton.addEventListener('click', refresh)
+
+function addComment(){
+
+	var commentBox = document.createElement('div')
+	var username = document.createElement('h5')
+	var commentText = document.createElement('span')
+
+	commentBox.classList.add('comment-wrapper')
+	commentBox.classList.add('comment')
+
+	username.innerHTML = 'Some User'
+	commentText.innerHTML = document.getElementById('new-comment-text').value
+
+	document.getElementById('new-comment-text').value = ""
+	commentBox.appendChild(username)
+	commentBox.appendChild(commentText)
+	commentSection.appendChild(commentBox)
+	
+	$("#comment-section").animate({ scrollTop: $('#comment-section').prop("scrollHeight")}, 500);
+
+
+}
+
+
+
+function refresh(){
+
+
+
+
+    $('#refresh-img').rotate({
+      angle: 0,
+      animateTo:180
+      })
+
+	$("#comment-section").animate({ scrollTop: $('#comment-section').prop("scrollHeight")}, 500);
+
+}
